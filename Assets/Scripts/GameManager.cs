3f20537e6c04f33cc306546d943bc0e921fc99d8
Irena <skrzynir@gmail.com>
2021-10-06 17:46:15 +1300

    Instatiate || Move

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    GameObject LocalPlayer = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+       LocalPlayer = Instantiate<GameObject>(Player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void PlayerMove()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<GameObject>().transform.position = Input.GetAxis("horizontal");
+        }
+    }
+}
